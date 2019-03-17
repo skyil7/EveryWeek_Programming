0a33d4da571125286066a35442b0759c34fa7a93
skyil7 <nakagimatv.2@gmail.com>
2019-03-17 21:17:13 +0900

    kakao Coding test

---
+++ KAKAO/1.py
@@ -0,0 +1,23 @@
+﻿"""
+카카오 코딩테스트 #1
+
+자연수 N이 주어지면, 
+N의 각 자릿수의 합을 구하시오.
+
+KaKao Coding Test # 1
+
+Given a natural number N,
+Find the sum of each digit of N.
+"""
+
+N = int(input("Given Number : "))
+
+remainder = 1
+sum = 0
+
+while remainder != 0 :
+    remainder = N % 10
+    N = (int)(N/10)
+    sum += remainder
+
+print("answer : ",sum)
